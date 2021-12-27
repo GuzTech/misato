d91ad939c047a0176100facb796cdbd9b8f8af3a
Oguz Meteer <info@guztech.nl>
2021-12-28 00:59:34 +0100

    Add formal verification to the decoder
    Signed-off-by: Oguz Meteer <info@guztech.nl>

--- decoder.py
+++ decoder.py
@@ -4,36 +4,39 @@ from amaranth.cli import main_parser, main_runner
 from amaranth.back import verilog
 from amaranth.sim import Simulator, Settle
 
+from amaranth.asserts import Assert, Assume, Cover
+
 from isa import *
 
 from typing import List
 
 
 class Decoder(Elaboratable):
-    def __init__(self, xlen: XLEN):
+    def __init__(self, xlen: XLEN, formal: bool):
         # Inputs
-        self.instr = Signal(xlen.value)
+        self.instr  = Signal(xlen.value)
 
         # Outputs
-        self.format = Signal(Format)
-        self.opcode = Signal(Opcode)
-        self.r_type = Signal()
-        self.i_type = Signal()
-        self.u_type = Signal()
-        self.s_type = Signal()
-        self.b_type = Signal()
-        self.j_type = Signal()
-        self.imm = Signal(xlen.value)
-        self.rs1 = Signal(5)
-        self.rs2 = Signal(5)
-        self.rd = Signal(5)
-        self.funct3 = Signal(Funct3)
-        self.funct7 = Signal(Funct7)
+        self.format  = Signal(Format)
+        self.opcode  = Signal(Opcode)
+        self.r_type  = Signal()
+        self.i_type  = Signal()
+        self.u_type  = Signal()
+        self.s_type  = Signal()
+        self.b_type  = Signal()
+        self.j_type  = Signal()
+        self.imm     = Signal(xlen.value)
+        self.rs1     = Signal(5)
+        self.rs2     = Signal(5)
+        self.rd      = Signal(5)
+        self.funct3  = Signal(Funct3)
+        self.funct7  = Signal(Funct7)
         self.u_instr = Signal(U_Type)
-        self.trap = Signal()
+        self.trap    = Signal()
 
         # Config
-        self.xlen = xlen.value
+        self.xlen    = xlen.value
+        self.formal  = formal
 
     def ports(self) -> List[Signal]:
         return [
@@ -61,16 +64,16 @@ class Decoder(Elaboratable):
 
         # Internal signals
         opcode = Signal(Opcode)
-        rd = Signal(5)
-        rs1 = Signal(5)
-        rs2 = Signal(5)
+        rd     = Signal(5)
+        rs1    = Signal(5)
+        rs2    = Signal(5)
         funct3 = Signal(Funct3)
         funct7 = Signal(Funct7)
-        i_imm = Signal(self.xlen)
-        s_imm = Signal(self.xlen)
-        b_imm = Signal(self.xlen)
-        u_imm = Signal(self.xlen)
-        j_imm = Signal(self.xlen)
+        i_imm  = Signal(self.xlen)
+        s_imm  = Signal(self.xlen)
+        b_imm  = Signal(self.xlen)
+        u_imm  = Signal(self.xlen)
+        j_imm  = Signal(self.xlen)
 
         # Extract bit-fields from the instruction
         m.d.comb += [
@@ -94,6 +97,17 @@ class Decoder(Elaboratable):
             self.funct3.eq(funct3),
             self.funct7.eq(funct7),
             self.opcode.eq(opcode),
+        ]
+
+        # Default values
+        m.d.comb += [
+            self.r_type.eq(0),
+            self.i_type.eq(0),
+            self.s_type.eq(0),
+            self.b_type.eq(0),
+            self.u_type.eq(0),
+            self.j_type.eq(0),
+            self.imm.eq(0),
         ]
 
         with m.Switch(opcode):
@@ -143,13 +157,118 @@ class Decoder(Elaboratable):
                     self.imm.eq(0),
                     self.trap.eq(1)
                 ]
+        
+        #
+        # Formal verification
+        #
+
+        if self.formal:
+            # Check if R-type instructions are correctly decoded
+            with m.If(self.opcode == Opcode.OP):
+                m.d.comb += [
+                    Assert(self.r_type == 0b1),
+                    Assert(self.i_type == 0b0),
+                    Assert(self.s_type == 0b0),
+                    Assert(self.b_type == 0b0),
+                    Assert(self.u_type == 0b0),
+                    Assert(self.j_type == 0b0),
+                    Assert(~self.trap),
+                    Assert(self.format == Format.R_type)
+                ]
+            with m.Else():
+                m.d.comb += Assert(self.r_type == 0b0)
+            
+            # Check if I-type instructions are correctly decoded
+            with m.If((self.opcode == Opcode.OP_IMM) | (self.opcode == Opcode.LOAD)):
+                m.d.comb += [
+                    Assert(self.r_type == 0b0),
+                    Assert(self.i_type == 0b1),
+                    Assert(self.s_type == 0b0),
+                    Assert(self.b_type == 0b0),
+                    Assert(self.u_type == 0b0),
+                    Assert(self.j_type == 0b0),
+                    Assert(self.imm == i_imm),
+                    Assert(~self.trap),
+                    Assert(self.format == Format.I_type)
+                ]
+            with m.Else():
+                m.d.comb += Assert(self.i_type == 0b0)
+
+            # Check if S-type instructions are correctly decoded
+            with m.If(self.opcode == Opcode.STORE):
+                m.d.comb += [
+                    Assert(self.r_type == 0b0),
+                    Assert(self.i_type == 0b0),
+                    Assert(self.s_type == 0b1),
+                    Assert(self.b_type == 0b0),
+                    Assert(self.u_type == 0b0),
+                    Assert(self.j_type == 0b0),
+                    Assert(self.imm == s_imm),
+                    Assert(~self.trap),
+                    Assert(self.format == Format.S_type)
+                ]
+            with m.Else():
+                m.d.comb += Assert(self.s_type == 0b0)
+
+            # Check if B-type instructions are correctly decoded
+            with m.If(self.opcode == Opcode.BRANCH):
+                m.d.comb += [
+                    Assert(self.r_type == 0b0),
+                    Assert(self.i_type == 0b0),
+                    Assert(self.s_type == 0b0),
+                    Assert(self.b_type == 0b1),
+                    Assert(self.u_type == 0b0),
+                    Assert(self.j_type == 0b0),
+                    Assert(~self.trap),
+                    Assert(self.imm == b_imm),
+                    Assert(self.format == Format.B_type)
+                ]
+            with m.Else():
+                m.d.comb += Assert(self.b_type == 0b0)
+
+            # Check if U-type instructions are correctly decoded
+            with m.If((self.opcode == Opcode.LUI) | (self.opcode == Opcode.AUIPC)):
+                m.d.comb += [
+                    Assert(self.r_type == 0b0),
+                    Assert(self.i_type == 0b0),
+                    Assert(self.s_type == 0b0),
+                    Assert(self.b_type == 0b0),
+                    Assert(self.u_type == 0b1),
+                    Assert(self.j_type == 0b0),
+                    Assert(~self.trap),
+                    Assert(self.imm == u_imm),
+                    Assert(self.format == Format.U_type)
+                ]
+            with m.Else():
+                m.d.comb += Assert(self.u_type == 0b0)
+
+            # Check if J-type instructions are correctly decoded
+            with m.If((self.opcode == Opcode.JAL) | (self.opcode == Opcode.JALR)):
+                m.d.comb += [
+                    Assert(self.r_type == 0b0),
+                    Assert(self.i_type == 0b0),
+                    Assert(self.s_type == 0b0),
+                    Assert(self.b_type == 0b0),
+                    Assert(self.u_type == 0b0),
+                    Assert(self.j_type == 0b1),
+                    Assert(~self.trap),
+                ]
+                with m.If(self.opcode == Opcode.JAL):
+                    m.d.comb += Assert(self.format == Format.J_type)
+                    m.d.comb += Assert(self.imm == j_imm)
+                with m.Else():
+                    m.d.comb += Assert(self.format == Format.I_type)
+                    m.d.comb += Assert(self.imm == i_imm)
+            with m.Else():
+                m.d.comb += Assert(self.j_type == 0b0)
 
         return m
 
 
 if __name__ == "__main__":
     m = Module()
-    m.submodules.decoder = decoder = Decoder(xlen=XLEN.RV32)
+    formal = True
+    m.submodules.decoder = decoder = Decoder(xlen=XLEN.RV32, formal=formal)
 
     def bench():
         # ADDI R31 = R1 + (-1)
@@ -160,14 +279,15 @@ if __name__ == "__main__":
         assert (yield decoder.i_type)
         assert not (yield decoder.trap)
 
-        yield decoder.instr.eq(-1)
-        yield Settle()
-        assert (yield decoder.trap)
+        # yield decoder.instr.eq(-1)
+        # yield Settle()
+        # assert (yield decoder.trap)
 
-    sim = Simulator(decoder)
-    sim.add_process(bench)
-    with sim.write_vcd("decoder.vcd"):
-        sim.run()
+    if not formal:
+        sim = Simulator(decoder)
+        sim.add_process(bench)
+        with sim.write_vcd("decoder.vcd"):
+            sim.run()
 
     with open("decoder.v", "w") as file:
         file.write(verilog.convert(m, ports=decoder.ports()))
