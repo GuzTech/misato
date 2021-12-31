1b27cf90bddafaf28738fd93d330f1e245f5b521
Oguz Meteer <info@guztech.nl>
2021-12-31 23:26:42 +0100

    Add a SoC class and change ulx3s.py to use it
    Signed-off-by: Oguz Meteer <info@guztech.nl>

---
+++ soc.py
@@ -0,0 +1,60 @@
+from amaranth import *
+from amaranth.cli import main_parser, main_runner
+from amaranth.build import Platform
+from amaranth.back import verilog
+from amaranth.sim import Simulator, Settle
+from amaranth_soc.wishbone import *
+
+from amaranth.asserts import *
+
+from cpu import *
+from rom import ROM
+from gpio import GPIO
+
+
+class SoC(Elaboratable):
+    def __init__(self, imem_init):
+        # Inputs
+
+        # Outputs
+        self.o_gpio    = Signal(32)
+        self.o_trap    = Signal()
+
+        # Configuration
+        self.imem_init = imem_init
+
+    def elaborate(self, platform: Platform) -> Module:
+        m = Module()
+
+        imem = Memory(width=32, depth=128, init=self.imem_init)
+        dmem = Memory(width=32, depth=128)
+
+        m.submodules.cpu    = cpu    = Misato(xlen=XLEN.RV32, with_RVFI=False)
+        m.submodules.gpio   = gpio   = GPIO()
+        m.submodules.imem_r = imem_r = imem.read_port()
+        m.submodules.dmem_r = dmem_r = dmem.read_port()
+        m.submodules.dmem_w = dmem_w = dmem.write_port()
+
+        # Connect cpu and instruction memory
+        m.d.comb += cpu.i_instr.eq(imem_r.data)
+        m.d.comb += imem_r.addr.eq(cpu.o_i_addr[2:])
+
+        # Connect cpu write to data bus arbiter
+        with m.If(cpu.o_d_addr == 0x80):
+            m.d.comb += gpio.i_w_en.eq(cpu.o_d_Wr)
+            m.d.comb += dmem_w.en.eq(0)
+        with m.Else():
+            m.d.comb += gpio.i_w_en.eq(0)
+            m.d.comb += dmem_w.en.eq(cpu.o_d_Wr)
+
+        # Connect cpu to data bus
+        m.d.comb += gpio.i_data.eq(cpu.o_d_data)
+        m.d.comb += dmem_r.addr.eq(cpu.o_d_addr)
+        m.d.comb += cpu.i_data.eq(dmem_r.data)
+        m.d.comb += dmem_w.addr.eq(cpu.o_d_addr)
+        m.d.comb += dmem_w.data.eq(cpu.o_d_data)
+
+        m.d.comb += self.o_gpio.eq(gpio.o_data)
+        m.d.comb += self.o_trap.eq(cpu.o_trap)
+
+        return m
--- ulx3s.py
+++ ulx3s.py
@@ -3,15 +3,9 @@ import subprocess
 import sys
 
 from amaranth import *
-from amaranth.build import Platform
-from amaranth.back import verilog
-from amaranth.sim import Simulator, Settle
-from amaranth_soc.wishbone import *
 from amaranth_boards.ulx3s import *
 
-from cpu import *
-from rom import ROM
-from gpio import GPIO
+from soc import SoC
 
 if __name__ == "__main__":
     variants = {
@@ -27,14 +21,16 @@ if __name__ == "__main__":
     parser.add_argument('program')
     args = parser.parse_args()
 
+    # Select the correct platform
     platform = variants[args.variant]()
 
+    # Assemble the program and extract the hex representation
     subprocess.run(["riscv64-elf-as", "-c", "programs/%s.s" % args.program, "-o", "programs/a.out"])
     subprocess.run(["riscv64-elf-objdump", "-d", "programs/a.out"])
     subprocess.run(["riscv64-elf-objcopy", "-O", "binary", "programs/a.out", "programs/hex.bin"])
 
+    # Fill the data list with the instructions (as hex)
     data = []
-
     with open("programs/hex.bin", "rb") as f:
         bindata = f.read()
 
@@ -42,10 +38,12 @@ if __name__ == "__main__":
         w = bindata[4*i : 4*i+4]
         data.append(int("%02x%02x%02x%02x" % (w[3], w[2], w[1], w[0]), 16))
 
+    # Build the SoC
     top = Module()
-    top.submodules.cpu  = cpu  = Misato(xlen=XLEN.RV32, with_RVFI=False)
-    top.submodules.gpio = gpio = GPIO()
+    top.submodules.soc = soc = SoC(imem_init=data)
 
+    # Connect the low 8-bit outputs of the GPIO
+    # to the LEDs on the ULX3S.
     leds = [platform.request("led", 0),
             platform.request("led", 1),
             platform.request("led", 2),
@@ -56,64 +54,6 @@ if __name__ == "__main__":
             platform.request("led", 7)]
 
     for i in range(len(leds)):
-        top.d.comb += leds[i].eq(gpio.o_data[i])
- 
-    # top.submodules.imem = mem = ROM(data)
-    # top.d.comb += cpu.ibus.connect(mem.arb.bus)
-
-    imem = Memory(width=32, depth=128, init=data)
-    # imem.attrs["ram_block"] = 1
-    top.submodules.imem_r = imem_r = imem.read_port()
-    top.d.comb += cpu.i_instr.eq(imem_r.data)
-    top.d.comb += imem_r.addr.eq(cpu.o_i_addr[2:])
-
-    dmem = Memory(width=32, depth=128)
-    # dmem.attrs["ram_block"] = 1
-    top.submodules.dmem_r = dmem_r = dmem.read_port()
-    top.submodules.dmem_w = dmem_w = dmem.write_port()
-    top.d.comb += dmem_r.addr.eq(cpu.o_d_addr)
-    top.d.comb += cpu.i_data.eq(dmem_r.data)
-    top.d.comb += dmem_w.addr.eq(cpu.o_d_addr)
-    # top.d.comb += dmem_w.en.eq(cpu.o_d_Wr)
-    top.d.comb += dmem_w.data.eq(cpu.o_d_data)
-
-    # Simple data bus arbiter
-    top.d.comb += gpio.i_data.eq(cpu.o_d_data)
-    with top.If(cpu.o_d_Wr & (cpu.o_d_addr == 0x80)):
-        top.d.comb += gpio.i_w_en.eq(cpu.o_d_Wr)
-        top.d.comb += dmem_w.en.eq(0)
-    with top.Else():
-        top.d.comb += gpio.i_w_en.eq(0)
-        top.d.comb += dmem_w.en.eq(cpu.o_d_Wr)
-
-
-    # We define our own "sync" clock domain so that we
-    # can access the reset signal for simulation.
-    # This does decouple the actual clock signal from
-    # our clock domain, so we have to manually connect
-    # the global clock to the clock domain. This does
-    # decrease the maximum clock speed a bit for some
-    # reason, so remove it when not needed.
-    sync = ClockDomain()
-    top.domains += sync
-    clk = platform.request(platform.default_clk)
-    top.d.comb += sync.clk.eq(clk)
-
-    def bench():
-        yield sync.rst.eq(1)
-        yield
-        yield sync.rst.eq(0)
-        yield
-        assert not (yield cpu.o_trap)
-        
-        for _ in range(200):
-            yield
-            assert not (yield cpu.o_trap)
-        
-    sim = Simulator(top)
-    sim.add_clock(1e-6)
-    sim.add_sync_process(bench)
-    with sim.write_vcd("cpu.vcd"):
-        sim.run()
+        top.d.comb += leds[i].eq(soc.o_gpio[i])
 
     platform.build(top, do_program=True, nextpnr_opts="--timing-allow-fail")
