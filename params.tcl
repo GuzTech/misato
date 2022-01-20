91069d3b86c0d7b3d806f21c28b0e888de66060a
Oguz Meteer <info@guztech.nl>
2022-01-21 00:34:21 +0100

    70% utilization, optimize for delay, 50 MHz
    Signed-off-by: Oguz Meteer <info@guztech.nl>

--- params.tcl
+++ params.tcl
@@ -1,6 +1,10 @@
-set ::env(CLOCK_PERIOD) "25"
+set ::env(CLOCK_PERIOD) "20"
 set ::env(CLOCK_PORT) "clk"
+set ::env(SYNTH_STRATEGY) "DELAY 0"
 set ::env(DESIGN_IS_CORE) 0
+
+set ::env(FP_CORE_UTIL) "70"
+set ::env(FP_ASPECT_RATIO) "1"
 
 set ::env(FP_SIZING) "absolute"
 set ::env(DIE_AREA) "0 0 500 500"
