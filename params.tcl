36e70b7907f965b6dafe9d838f9b11177ef1ef8d
Oguz Meteer <info@guztech.nl>
2022-01-20 23:01:08 +0100

    Adjust area and clock period (40 MHz) for sky130
    Signed-off-by: Oguz Meteer <info@guztech.nl>

--- params.tcl
+++ params.tcl
@@ -1,6 +1,6 @@
-set ::env(CLOCK_PERIOD) "10"
+set ::env(CLOCK_PERIOD) "25"
 set ::env(CLOCK_PORT) "clk"
 set ::env(DESIGN_IS_CORE) 0
 
 set ::env(FP_SIZING) "absolute"
-set ::env(DIE_AREA) "0 0 1000 1000"
+set ::env(DIE_AREA) "0 0 500 500"
